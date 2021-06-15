aba00cd639a96e3d1d077725cd918760ceafb0f5
sureshmangs <sureshmangs1@gmail.com>
2021-06-16 00:50:39 +0530

    creating footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="bg-dark text-center text-white pt-5 pb-5">
+                <p className="mt-5">Made with <i className="fa fa-heart" style={{ color: "#FF5252" }}></i> </p>
+
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
