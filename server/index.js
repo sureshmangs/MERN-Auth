af634e73557a23965d92d4d6b6a6c685cd62946a
sureshmangs <sureshmangs1@gmail.com>
2021-07-11 22:26:50 +0530

    adding user routes, adding morgan middleware, adding form parsing middleware,

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
 const connectDB = require('./connectDB.js');
 
 const app = express();
 
+// Middlewares
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// To parse the incoming requests with JSON payloads
+app.use(express.json());
+
+// HTTP request logger middleware 
+app.use(morgan('tiny'));
 
 // Connect to Database
 connectDB();
+
+// Routes
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
