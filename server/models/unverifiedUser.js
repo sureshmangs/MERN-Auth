c9c68e03e5fb71d4e9d158d2f6f6623b19bdb76a
sureshmangs <sureshmangs1@gmail.com>
2021-07-11 22:23:46 +0530

    creating unverified_user schema

---
+++ server/models/unverifiedUser.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const UnverifiedUser = mongoose.model('unverified_user', userSchema);
+
+module.exports = UnverifiedUser;
